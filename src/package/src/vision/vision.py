d7f62dc7dbd4bcc531bc627260bad5dcc472b186
carlottacazzolli <carlottacazzolli02@gmail.com>
2024-02-11 15:25:23 +0100

    orientation vector

--- src/package/src/vision/vision.py
+++ src/package/src/vision/vision.py
@@ -50,6 +50,7 @@ def handle_coordinates(req):
 def transform_vector(vector):
     m= np.zeros((4, 4))
     vector_translation = vector[:3]
+    orientation_part = vector[3:]
     m[0, 0] = 1
     m[0, 1] = 0
     m[0, 2] = 0
@@ -72,7 +73,8 @@ def transform_vector(vector):
 
     homogeneous_vector = np.append(vector_translation, 1)
     transformed_vector = np.dot(m, homogeneous_vector)
-    return transformed_vector[:-1]
+    transform_vector=transformed_vector[:-1]
+    return transformed_vector+orientation_part
 
 
 if __name__ == "__main__":
