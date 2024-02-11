3f53c33107777573fdda4477977d1c33f39c97f8
martinapanini <martina.panini@studenti@unitn.it>
2024-02-11 15:15:21 +0100

    test

--- src/package/src/tests/test_node.cpp
+++ src/package/src/tests/test_node.cpp
@@ -1,23 +1,24 @@
 #include <ros/ros.h>
+#include <roboticProject/coordinate.h> 
+#include <iostream>
 
-int main(int argc, char **argv)
-{
+int main(int argc, char** argv) {
     ros::init(argc, argv, "test_node");
     ros::NodeHandle nh;
 
+    // Simulazione della chiamata del servizio al nodo vision
+    ros::ServiceClient client = nh.serviceClient<coordinate::Coordinate>("vision");
 
-    ros::ServiceClient client = nh.serviceClient<block_detection::Coordinate>("get_coordinates");
-
-    block_detection::Coordinate srv;
-    srv.request.block_id = 1; 
-
-    if (client.call(srv)) // Invia la richiesta di coordinate
-    {
-        ROS_INFO("Coordinate ricevute: x=%f, y=%f, z=%f", srv.response.x, srv.response.y, srv.response.z);
-    }
-    else
-    {
-        ROS_ERROR("Impossibile ottenere le coordinate");
+    // Creazione di un oggetto Coordinate
+    coordinate::poseStart srv;
+   
+    // Chiamata al servizio
+    if (client.call(srv)) {
+        ROS_INFO("Chiamata al servizio avvenuta con successo");
+        // Stampa delle coordinate acquisite
+        std::cout << "Coordinate acquisite: x = " << srv.response.poseStart<< std::endl;
+    } else {
+        ROS_ERROR("Errore durante la chiamata al servizio");
         return 1;
     }
 
